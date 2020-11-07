eb000f49979a4a2b7868b823c796f55a9496a78c
pepe <diabhlo@gmail.com>
2020-11-07 10:00:00 -0300

    listo para el heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_06_170949) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.boolean "public"
